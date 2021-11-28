9596f012f3a905fde6531c2de5ddb040ee32a6c2
Eric Honorez <eric.honorez@gmail.com>
2021-11-28 19:28:26 +0100

    Changed: cleanup some files

--- src/main/java/org/h5z/jval/Data.java
+++
@@ -1,7 +0,0 @@
-package org.h5z.jval;
-
-public class Data {
-    // Data API to validation
-    // in order to be able to programmatically generate validation
-    // Derive validation from Object shape
-}
